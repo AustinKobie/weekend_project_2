425e180be4467bba2adcbf7475839e8ae2a25832
AustinKobie <110858945+AustinKobie@users.noreply.github.com>
2023-02-03 13:18:47 -0500

    First Commit
    Base commit for weekend project #2

---
+++ weekend_project_2.py
@@ -0,0 +1,16 @@
+class Parking_Garage:
+    def __init__(self):
+        self.take_ticket = []
+        self.parking_Spaces = []
+        
+    
+    
+    def take_ticket(self):
+        self.take_ticket.pop()
+        
+        
+        
+    def pay_for_parking_ticket(self):
+        
+    
+    def leave_garage(self):
