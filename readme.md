14c4d81ec2845b0350bb9c49d10b305174266824
AustinKobie <110858945+AustinKobie@users.noreply.github.com>
2023-02-05 20:16:29 -0500

    merge and bug fixes
    Merged codes tonight and had a few bugs, we fixed those over a few hours.
    Would like to fix one more thing before submission involving carrying over the remainder of owed currency.

---
+++ readme.md
@@ -0,0 +1,3 @@
+Austin - Lines 25-31
+Leandro - Lines 34-49
+Start of project and revisions/bug checking done by both contributors over video call. 
--- weekend_project_2.py
+++ weekend_project_2.py
@@ -1,45 +1,56 @@
 class Parking_Garage:
     def __init__(self):
-        self.take_ticket = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-        self.parking_Spaces = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-        self.paid_ticket = {'paid': False, 'ticket': None}
-    
-# Ask user if they would like to enter the garage in no then end program if yes then
-#give a ticket to user unless we are out of tickets/spaces.
-    def take_ticket(self):
-        enter_garage = input("Would you like to enter the garage? (y/n):").lower()
-        if True == "y":
-            if self.take_ticket:
-                ticket = self.take_ticket.pop()
+        self.ticket_supply = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+        self.parking_spaces = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+        self.paid_ticket = {'paid': False,
+                            'ticket': None
+                            }
+
+
+    def issue_ticket(self):
+        enter_garage = input(
+            "Would you like to enter the garage? (y/n):").lower()
+        if enter_garage == "y":
+            if self.ticket_supply:
+                ticket = self.ticket_supply.pop()
                 self.paid_ticket['ticket'] = ticket
-                print("Your ticket number is {ticket}.")
+                print(f"Your ticket number is {ticket}. Please pay $5.00")
             else:
                 print("No more tickets available at this time.")
         else:
-            print("Are you sure you would like to find a space elsewhere?")
-            
-#as the user for payment, they will need to input the ammount
-# If the payment variable is not empty then (meaning the ticket has been paid) -> 
-# display a message to the user that their ticket has been paid and they have 15mins to leave        
-#  This should update the "currentTicket" dictionary key "paid" to True     
+            print("If you still need a spot, we'll be here.")
+            return
+        
+        
     def pay_for_parking_ticket(self):
-        take_payment = input("Please enter a payment amount.")
-        if take_payment:
-            print("Your payment has been received. Please leave in the next 15 minutes.")
-            
- # take payment from user for ticket and ask them to leave garage
- #if ticket has not neen paid prompt user for payment
- # once ticket is paid display a message thanking user   
+        take_payment = input("Please enter payment amount.")
+        payment = float(take_payment)
+        if payment >= 5.0:
+            change = payment - 5.0
+            print(f"Your payment has been received. Please leave in the next 15 minutes. Thank you for the ${change} tip!!")
+            self.paid_ticket['paid'] = True
+
+
     def leave_garage(self):
-        if self.paid_ticket['paid'] = True
+        if self.paid_ticket['paid'] == True:
             print("Thank you, have a nice day!")
             self.paid_ticket['paid'] = False
+            self.parking_spaces.append(self.paid_ticket['ticket'])
+            self.paid_ticket['ticket'] = None
         else:
-            print("Payment is now due"):
-            payment_due= input("Please pay 5.00")
-            if take_payment:
-            print("Your payment has been received. Please leave in the next 15 minutes.")
+            print("Payment is now due")
+            payment_due = input("Please pay $5.00")
+            payment = float(payment_due)
+            if payment:
+                print(
+                    "Your payment has been received. Please leave in the next 15 minutes.")
+                self.parking_spaces.append(self.paid_ticket['ticket'])
+                self.paid_ticket['paid'] = False
+                self.paid_ticket['ticket'] = None
 
-            self.parking_Spaces.append(self.paid_ticket['ticket'])
-            self.parking_Spaces.append(self.parking_Spaces)
 
+parking_garage = Parking_Garage()
+parking_garage.issue_ticket()
+if parking_garage.paid_ticket['ticket']:
+    parking_garage.pay_for_parking_ticket()
+    parking_garage.leave_garage()
